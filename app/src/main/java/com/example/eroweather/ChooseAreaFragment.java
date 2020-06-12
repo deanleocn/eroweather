d693b3be1447a9ef9dec414dfbefd1f238fd2ca2
deanleocn <deanleocn@hotmail.com>
2020-06-12 22:10:24 +0800

    Finish basic layout and function

--- app/src/main/java/com/example/eroweather/ChooseAreaFragment.java
+++ app/src/main/java/com/example/eroweather/ChooseAreaFragment.java
@@ -1,5 +1,6 @@
 package com.example.eroweather;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -76,6 +77,12 @@ public class ChooseAreaFragment extends Fragment {
                 } else if (currentLevel == LEVEL_CITY) {
                     selectedCity = cityList.get(position);
                     queryCounties();
+                } else if (currentLevel == LEVEL_COUNTY) {
+                    String weatherId = countyList.get(position).getWeatherId();
+                    Intent intent = new Intent(getActivity(), WeatherActivity.class);
+                    intent.putExtra("weather_id", weatherId);
+                    startActivity(intent);
+                    getActivity().finish();
                 }
             }
         });
--- app/src/main/java/com/example/eroweather/MainActivity.java
+++ app/src/main/java/com/example/eroweather/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.eroweather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SharedPreferences prefs = this.getSharedPreferences(WeatherActivity.FILE_NAME, MODE_PRIVATE);
+        if (prefs.getString("weather", null) != null) {
+            Intent intent = new Intent(this, WeatherActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
